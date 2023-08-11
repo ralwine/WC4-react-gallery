1e8aa4a8e25975db0cac26028ecb177ba732dfc8
Ryan M Alwine <ryanmitchellalwine@gmail.com>
2023-08-11 18:00:11 -0500

    added images to folder, dependencies hooked

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,6 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Louie has a penpal from Scotland. His name is Bruce', likes: 0 },
+    { id: 2, path: 'images/louie_window.jpeg', description: 'Louie stares intently out of the window. He is the guardian of the home!', likes: 0 }
 ];
 
 module.exports = galleryItems;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import Axios from 'axios';
 import './App.css';
 
 function App() {
+
+  // GET in here!
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
+          <h1 className="App-title">Gallery of My Louie</h1>
         </header>
         <p>Gallery goes here</p>
         <img src="images/goat_small.jpg"/>
+        <img src="images/louie_window.jpeg"/>
       </div>
     );
 }
