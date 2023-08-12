02a2c7c2b19928596284dd54ac9b38dccef5e9e1
Ryan M Alwine <ryanmitchellalwine@gmail.com>
2023-08-11 19:41:10 -0500

    GET route working in POSTMAN

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,6 +1,11 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Louie has a penpal from Scotland. His name is Bruce', likes: 0 },
-    { id: 2, path: 'images/louie_window.jpeg', description: 'Louie stares intently out of the window. He is the guardian of the home!', likes: 0 }
+    { id: 11, path: 'images/goat_small.jpg', description: 'Louie has a penpal from Scotland. His name is Bruce', likes: 0 },
+    { id: 1, path: 'images/louie_window.jpeg', description: 'Louie stares intently out of the window. He is the guardian of the home!', likes: 0 },
+    { id: 2, path: 'images/king_louie.jpeg', description: 'Louie stares intently out of the window. He is the guardian of the home!', likes: 0 },
+    { id: 3, path: 'images/louie_snuggle1.jpeg', description: 'Louie stares intently out of the window. He is the guardian of the home!', likes: 0 },
+    { id: 4, path: 'images/louie_snuggle2.jpeg', description: 'Louie stares intently out of the window. He is the guardian of the home!', likes: 0 },
+    { id: 5, path: 'images/louie+mom.jpeg', description: 'Louie stares intently out of the window. He is the guardian of the home!', likes: 0 }
+   
 ];
 
 module.exports = galleryItems;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import './App.css';
+import axios from 'axios';
 
 function App() {
 
+  let [GalleryList, setGalleryList] = useState([]);
+
+  useEffect(() =>{
+    getGallery();
+  })
+
   // GET in here!
+  const getGallery = () =>{
+    axios
+      .get('/gallery')
+      .then((response)=>{
+        setGalleryList(response.data);
+      })
+      .catch((error) => {
+        alert(`Failed to retrieve images.`)
+        console.log('Error getting images in APP', error)
+      })
+  }
+
     return (
       <div className="App">
         <header className="App-header">
