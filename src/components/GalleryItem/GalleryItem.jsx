5bdfcf6bf5117c3192e57463fb5fed2d5cf96284
Ryan M Alwine <ryanmitchellalwine@gmail.com>
2023-08-13 15:51:36 -0500

    like button added and responding

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -2,19 +2,23 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 //import GalleryList from '../GalleryList/GalleryList.jsx';
 
-function GalleryItem(props)   {
-    
+function GalleryItem(props) {
+
     const imageClick = () => {
         console.log('Image clicked!')
     }
-    
+    // now how do we get this image to change state?
+
+    const likeClick = () =>{
+        console.log('Liked')
+    }
     return (
-        
-        <img onClick= {imageClick} src={props.galleryItems.path} />
-
-
+        <>
+            <img onClick={imageClick} src={props.galleryItems.path} />
+            <button onClick={likeClick}>Like🤙</button>
+        </>
     );
-   
+
 }
 
 export default GalleryItem;
